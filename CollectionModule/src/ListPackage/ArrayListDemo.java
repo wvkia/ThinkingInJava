5d6f6b6dbed600056a0b17e3d50040db79d95fee
wvkia <502332082@qq.com>
2018-03-22 23:23:18 +0800

    arraylist word

---
+++ CollectionModule/src/ListPackage/ArrayListDemo.java
@@ -0,0 +1,10 @@
+package ListPackage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+    }
+}
